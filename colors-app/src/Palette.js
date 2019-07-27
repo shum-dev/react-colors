4c62193948360ae4f66666f31139fbcf1fea3b9c
Egor Shum <89135509130@mail.ru>
2019-07-27 15:10:24 +0700

    Initial commit with created react app, added palette and seedColors.js

---
+++ colors-app/src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Palete extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        <div className='Palette-coolors'>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Palete;
