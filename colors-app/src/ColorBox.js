61245326eee22e7a38d041c97dd1b4681ee77fe8
Egor Shum <89135509130@mail.ru>
2019-07-31 14:48:59 +0700

    Adjust text color based on luminance

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
+import './ColorBox.css';
+
 
 class ColorBox extends Component {
   constructor(props) {
@@ -19,6 +21,7 @@ class ColorBox extends Component {
   render() {
     const {name, background, paletteId, id, hideLink} = this.props;
     const {copied} = this.state;
+    const isDarkColor = chroma(background).luminance() <= 0.2;
     return (
       <CopyToClipboard
         text={background}
@@ -31,18 +34,18 @@ class ColorBox extends Component {
           />
           <div className={`copy-msg ${copied && 'show'}`}>
             <h1>copied!</h1>
-            <p>{this.props.background}</p>
+            <p className={!isDarkColor && 'dark-text'}>{this.props.background}</p>
           </div>
           <div className='copy-container'>
             <div className='box-content'>
-              <span>{name}</span>
+              <span className={isDarkColor && 'light-text'}>{name}</span>
             </div>
-            <button className='copy-button'>Copy</button>
+            <button className={`copy-button ${!isDarkColor && 'dark-text'}`}>Copy</button>
           </div>
           {
             !hideLink &&
             <Link to={`/palette/${paletteId}/${id}`} onClick={(e)=>e.stopPropagation()}>
-              <span className='see-more'>More</span>
+              <span className={`see-more ${!isDarkColor && 'dark-text'}`}>More</span>
             </Link>
           }
 
